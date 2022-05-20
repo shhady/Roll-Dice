dc8fb3d0283d87e44c91d0624c2e6d16c5399e54
shhady <shhadyse@gmail.com>
2022-05-20 15:17:40 +0300

    winner

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -5,11 +5,12 @@ import image from "./images/background.jpg";
 import MainTest from "./players";
 import Player from "./players"
 class Main extends React.Component {
+    
     state = {player1name:"player1", player2name:"player2", player1: true, player2: false, totalScore1: 0, currentScore1:0,totalScore2: 0, currentScore2:0, imgNum1: 0, imgNum2:0} 
-   
+
 
     resetGame = ()=>{
-        this.setState({currentScore1: 0, currentScore2: 0, totalScore2: 0,totalScore1: 0,})
+        this.setState({currentScore1: 0, currentScore2: 0, totalScore2: 0,totalScore1: 0})
     }
     randomizeNumbers = ()=>{
     const rand1 = Math.ceil(Math.random()*6)
@@ -19,27 +20,27 @@ class Main extends React.Component {
     if(this.state.player1 === true){
     this.setState({currentScore1: this.state.currentScore1 + rand1+ rand2}, ()=>{
         this.setState((prevState)=>({totalScore1:prevState.totalScore1 + rand1+ rand2}))
-        if(this.state.totalScore1 > 99){
-            this.setState({player2name: "Winner Winner"})
-        } 
-    })
+       if(this.state.currentScore1 > 100){
+           this.setState({player2name: "Winner Winner", currentScore1: 0, currentScore2: 0, totalScore2: 0,totalScore1: 0})
+       }else if(this.state.currentScore1 === 100){
+        this.setState({player1name: "Winner Winner", currentScore1: 0, currentScore2: 0, totalScore2: 0,totalScore1: 0})
+       }
+    }) 
     }  
     else if(this.state.player2 === true) 
     
     this.setState({currentScore2: this.state.currentScore2 + rand1+ rand2}, ()=>{
         this.setState((prevState)=>({totalScore2:prevState.totalScore2 + rand1+ rand2}))
-        if(this.state.totalScore2 > 100){
-            this.setState({player1name: "Winner Winner"})
-        }
+        if(this.state.currentScore2 > 100){
+            this.setState({player1name: "Winner Winner", currentScore1: 0, currentScore2: 0, totalScore2: 0,totalScore1: 0})
+        } else if(this.state.currentScore2 === 100){
+            this.setState({player2name: "Winner Winner", currentScore1: 0, currentScore2: 0, totalScore2: 0,totalScore1: 0})
+           }
     })
     
    }
    onClickHold =() =>{
     this.setState({player1: !this.state.player1 , player2: !this.state.player2})
-    // if(this.state.player1 === true){
-    //     this.setState({totalScore1: this.state.totalScore1})
-//    }
-    // this.setState({totalScore2: this.state.currentScore2 + rand1+ rand2})
     this.setState({currentScore2: 0})
     this.setState({currentScore1: 0})
    }
@@ -55,11 +56,6 @@ class Main extends React.Component {
         <div><button className="reset" onClick={this.resetGame}>Reset Game</button></div>
         </div>
         <div className="mainGame">
-        {/* <div className="player1" style={{ backgroundImage:`url(${image})` }}>
-        <h2>{this.state.players[0]}</h2>
-        <div>Total score</div>
-        <div>Current score</div>
-        </div> */}
         <Player name={this.state.player1name} totalScore={this.state.totalScore1} currentScore={this.state.player1 && this.state.currentScore1}/>
         <div className="tools">
         <img src={require(`./images/pic-${this.state.imgNum1}.png`)} style={{width:50}}/>
@@ -68,12 +64,6 @@ class Main extends React.Component {
         <button className="hold-button" onClick={this.onClickHold}>HOLD</button>
         <input type = 'number' className="input-button"></input>
         </div>
-        {/* <div className="player2" style={{ backgroundImage:`url(${image})` }}>
-            <h2>{this.state.players[0]}</h2>
-            <div>Total score</div>
-        <div>Current score</div>
-        </div> */}
-         
         <Player className="player1" name={this.state.player2name} totalScore={this.state.totalScore2} currentScore={this.state.player2 && this.state.currentScore2}/>
         </div>
     </div>
