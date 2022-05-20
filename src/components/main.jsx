8dc01ff91c10b8156d4519ad14f1d59cc23dc8d3
shhady <shhadyse@gmail.com>
2022-05-20 20:54:00 +0300

    maxScoreInput

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -5,7 +5,7 @@ import React from "react";
 import Player from "./players"
 class Main extends React.Component {
     
-    state = {player1name:"player1", player2name:"player2", player1: true, player2: false, totalScore1: 0, currentScore1:0,totalScore2: 0, currentScore2:0, imgNum1: 0, imgNum2:0, winnerShow: true} 
+    state = {player1name:"player1", player2name:"player2", player1: true, player2: false, totalScore1: 0, currentScore1:0,totalScore2: 0, currentScore2:0, imgNum1: 0, imgNum2:0, winnerShow: true, maxScore: 100} 
 
 
     resetGame = ()=>{
@@ -19,10 +19,10 @@ class Main extends React.Component {
     if(this.state.player1 === true){
     this.setState({currentScore1: this.state.currentScore1 + rand1+ rand2}, ()=>{
         this.setState((prevState)=>({totalScore1:prevState.totalScore1 + rand1+ rand2}))
-       if(this.state.currentScore1 > 100){
-           this.setState({player2name: "player 2 is the Winner Winner",player1name:`player 1 total score is ${this.state.currentScore1}`,  winnerShow: false})
-       }else if(this.state.currentScore1 === 100){
-        this.setState({player1name: "player 1 is the Winner Winner he reached 100",player2name:"", currentScore1: 0, currentScore2: 0, totalScore2: 0,totalScore1: 0, winnerShow: false})
+       if(this.state.currentScore1 > this.state.maxScore){
+           this.setState({player2name: "player 2 is the Winner",player1name:`player 1 total score is ${this.state.currentScore1}`,  winnerShow: false})
+       }else if(this.state.currentScore1 === this.state.maxScore){
+        this.setState({player1name: "player 1 is the Winner he reached 100",player2name:"", currentScore1: 0, currentScore2: 0, totalScore2: 0,totalScore1: 0, winnerShow: false})
        }
     }) 
     }  
@@ -30,20 +30,23 @@ class Main extends React.Component {
     
     this.setState({currentScore2: this.state.currentScore2 + rand1+ rand2}, ()=>{
         this.setState((prevState)=>({totalScore2:prevState.totalScore2 + rand1+ rand2}))
-        if(this.state.currentScore2 > 100){
-            this.setState({player1name: "player 1 is the Winner Winner",player2name:`player 2 total score is ${this.state.currentScore2}`,  winnerShow: false})
-        } else if(this.state.currentScore2 === 100){
-            this.setState({player2name: "player 2 is the Winner Winner he reached 100",player1name: "", currentScore1: 0, currentScore2: 0, totalScore2: 0,totalScore1: 0,  winnerShow: false})
+        if(this.state.currentScore2 > this.state.maxScore){
+            this.setState({player1name: "player 1 is the Winner",player2name:`player 2 total score is ${this.state.currentScore2}`,  winnerShow: false})
+        } else if(this.state.currentScore2 === this.state.maxScore){
+            this.setState({player2name: "player 2 is the Winner he reached 100",player1name: "", currentScore1: 0, currentScore2: 0, totalScore2: 0,totalScore1: 0,  winnerShow: false})
         }
     })
     
+
    }
    onClickHold =() =>{
     this.setState({player1: !this.state.player1 , player2: !this.state.player2})
     this.setState({currentScore2: 0})
     this.setState({currentScore1: 0})
    }
-  
+  onInputChange = (event)=>{
+      this.setState({maxScore: event.target.value})
+  }
     render(){
   return (
     <div className="main">
@@ -61,11 +64,19 @@ class Main extends React.Component {
         <img src={require(`./images/pic-${this.state.imgNum2}.png`)} alt="dice" style={{width:50}}/>
         <button className="roll-dice-button" onClick={this.randomizeNumbers}>Roll The Dice</button>
         <button className="hold-button" onClick={this.onClickHold}>HOLD</button>
-        <input type = 'number' className="input-button"></input>
+        <label style={{color: "white"}}>MAX SCORE</label>
+        <input type = 'number' className="input-button" onChange={this.onInputChange}></input>
         </div>
         <Player className="player1" name={this.state.player2name} totalScore={this.state.totalScore2} currentScore={this.state.player2 && this.state.currentScore2}/>
         </div>}
-        {this.state.winnerShow || <div className="winnerwinner"><h1>{this.state.player1name} {this.state.player2name}</h1></div>}
+        {this.state.winnerShow || <div className="winnerwinner">
+                 <div>
+                     <h1>{this.state.player1name}</h1>
+                 </div> 
+                 <div>
+                     <h1>{this.state.player2name}</h1>
+                </div>
+            </div>}
     </div>
   );
 }
